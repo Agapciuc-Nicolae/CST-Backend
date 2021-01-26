314a667a3452e7eef9e9679a7fcfce37f6aef2fb
Agapciuc-Nicolae <73312525+Agapciuc-Nicolae@users.noreply.github.com>
2021-01-26 16:41:54 +0200

    Add project files.

---
+++ ViverBackend/Entities/ViverContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViverBackend.Entities.Models;
+
+namespace ViverBackend.Entities
+{
+    public class ViverContext : DbContext
+    {
+        public ViverContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+ 
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.FollowedUsers)
+                .WithOne(fu => fu.Follows)
+                .HasForeignKey(fu => fu.FollowsId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+ 
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.FollowsUsers)
+                .WithOne(fu => fu.FollowedBy)
+                .HasForeignKey(fu => fu.FollowedById)
+                .OnDelete(DeleteBehavior.Restrict);
+
+ 
+
+            modelBuilder.Entity<User>()
+                .HasQueryFilter(u => !u.IsDeleted);
+        }
+    }
+}
